b5e192723cd8d8ee6480d232a7802bbf4b2d78d2
Baddula Chakravardhan <149646942+Chakravardhan-B@users.noreply.github.com>
2025-08-08 10:30:40 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from sklearn.metrics import balanced_accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import balanced_accuracy_score, f1_score, precision_score, recall_score, classification_report
 from tqdm import tqdm, tqdm_gui,tqdm_notebook
 from PerClassEvaluation import PerClassEvaluation
 from ROC_DCA import roc
@@ -26,19 +26,24 @@ def test_model(model, device, val_loader):
 
     print(f'Testing Accuracy: {100 * correct / total}%')
     Preds = np.concatenate(Preds)
-    Probs = np.concatenate(Probs)
     Labels = np.concatenate(Labels)
-    TestingMetrics.append(PerClassEvaluation(Preds, Labels))
-    bacc = balanced_accuracy_score(Labels, Preds)
-    pre = precision_score(Labels, Preds, average='weighted')
-    rec = recall_score(Labels, Preds, average='weighted')
-    f1 = f1_score(Labels, Preds, average='weighted')
-    tpr, fpr, auc = roc(Labels, Probs)
-    auc = [auc]
+    report = classification_report(Labels, Preds, digits = 4)
+    return report
+    
+    # Preds = np.concatenate(Preds)
+    # Probs = np.concatenate(Probs)
+    # Labels = np.concatenate(Labels)
+    # TestingMetrics.append(PerClassEvaluation(Preds, Labels))
+    # bacc = balanced_accuracy_score(Labels, Preds)
+    # pre = precision_score(Labels, Preds, average='weighted')
+    # rec = recall_score(Labels, Preds, average='weighted')
+    # f1 = f1_score(Labels, Preds, average='weighted')
     # tpr, fpr, auc = roc(Labels, Probs)
-    TestingMetricsALL.append([100 * correct / total, bacc * 100, pre * 100, rec * 100, f1 * 100])
+    # auc = [auc]
+    # # tpr, fpr, auc = roc(Labels, Probs)
+    # TestingMetricsALL.append([100 * correct / total, bacc * 100, pre * 100, rec * 100, f1 * 100])
 
-    return TestingMetrics, TestingMetricsALL, tpr, fpr, auc
+    # return TestingMetrics, TestingMetricsALL, tpr, fpr, auc
 
 
 def test_SavedModel(model, device, val_loader):
@@ -65,5 +70,6 @@ def test_SavedModel(model, device, val_loader):
     Probs = np.concatenate(Probs)
     Labels = np.concatenate(Labels)
     tpr, fpr, auc = roc(Labels, Probs)
+
 
     return tpr, fpr , auc
